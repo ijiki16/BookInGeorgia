31594e45812535f937b316142a806a7a2359a3d2
Devi <dkhos17@freeuni.edu.ge>
2019-07-12 22:36:42 +0400

    add comments in HotelsDB

--- src/DataBases/MyDBInfo.java
+++ src/DataBases/MyDBInfo.java
@@ -6,6 +6,8 @@ public class MyDBInfo {
 	public static final String MYSQL_PASSWORD = "Devi0406";
 	public static final String MYSQL_DATABASE_SERVER = "localhost:3306/"
 			+ "booking?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	
+	/** The Constant MYSQL_DATABASE_NAME. */
 	public static final String MYSQL_DATABASE_NAME = "booking";
 }
 
