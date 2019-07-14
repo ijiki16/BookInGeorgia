da6c2aa937949e5615069c3fe0d0fa9bcdfb64a3
Sandro Pertaia <apert17@freeuni.edu.ge>
2019-07-14 13:21:29 +0400

    upload default image

--- src/DataBases/MyDBInfo.java
+++ src/DataBases/MyDBInfo.java
@@ -1,13 +1,10 @@
 package DataBases;
 
-public class MyDBInfo {
-	
+public class MyDBInfo {	
 	public static final String MYSQL_USERNAME = "root";
 	public static final String MYSQL_PASSWORD = "Sandro568552663";
 	public static final String MYSQL_DATABASE_SERVER = "localhost:3306/"
 			+ "booking?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	
-	/** The Constant MYSQL_DATABASE_NAME. */
 	public static final String MYSQL_DATABASE_NAME = "booking";
 }
 
